477b35e1c20cef5d69a3b1bc8f4661fa66991444
Rony Ormandy Ortiz Alvarez <ormandyrony@gmail.com>
2023-03-27 20:25:04 -0600

    eliminando la api para hacer un merge

--- Proyecto_1/API/Models/parametrosFiltro.cs
+++
@@ -1,8 +0,0 @@
-public class parametrosFiltro
-{
-    public IEnumerable<string> usuarios { get; set; }
-
-    public IEnumerable<string> fechas { get; set; }
-
-    public IEnumerable<string> grupos { get; set; }
-}
