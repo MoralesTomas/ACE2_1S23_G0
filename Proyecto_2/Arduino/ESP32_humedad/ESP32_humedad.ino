fa4046016dba5ca3fe811cd1dd29a956dc844d0d
Rony Ormandy Ortiz Alvarez <ormandyrony@gmail.com>
2023-04-23 10:24:38 -0600

    Codigo para lectura del sensor de humedad en el suelo

---
+++ Proyecto_2/Arduino/ESP32_humedad/ESP32_humedad.ino
@@ -0,0 +1,16 @@
+const int Analog_channel_pin= 15;
+int ADC_VALUE = 0;
+
+void setup() 
+{
+Serial.begin(115200);
+}
+void loop() 
+{
+ADC_VALUE = analogRead(Analog_channel_pin);
+Serial.print("ADC VALUE = ");
+Serial.println(ADC_VALUE);
+delay(1000);
+
+delay(1000);
+}
